d4414e8216cbf4c4d7d572a08e3ab944660a125d
sanginiladia <43060844+sanginiladia@users.noreply.github.com>
2019-04-10 00:53:01 +0530

    Add files via upload

---
+++ week1_prog3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b,c,d,e,f,g,exp;
+	cout<<"a: ";
+	cin>>a;
+	cout<<"b: ";
+	cin>>b;
+	cout<<"c: ";
+	cin>>c;
+	cout<<"d: ";
+	cin>>d;
+	cout<<"e: ";
+	cin>>e;
+	cout<<"f:  ";
+	cin>>g;
+	exp=((a+b/c*d-e)*(f-g));
+	cout<<exp;
+	return 0;
+}
